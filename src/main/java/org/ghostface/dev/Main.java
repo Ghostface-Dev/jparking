bcd3a8118b08089f1409ebd0a68103492a045079
ghost_face_dev <henriquedev6@gmail.com>
2024-07-31 20:18:05 -0300

    improved code

--- src/main/java/org/ghostface/dev/Main.java
+++ src/main/java/org/ghostface/dev/Main.java
@@ -1,25 +1,12 @@
 package org.ghostface.dev;
 
 import org.ghostface.dev.impl.Client;
+import org.ghostface.dev.impl.data.CustomerRecord;
 import org.ghostface.dev.impl.parking.ParkingSystem;
 import org.ghostface.dev.impl.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
-
-        ParkingSystem system = new ParkingSystem(15);
-        System.out.println(system.spotsAvailable());
-
-        Vehicle vehicle = new Vehicle(2, "SWS", "corsa", "ford", "red");
-        Client client = new Client(1, "Henrique", "555", "@gmail.com", vehicle);
-
-        system.reserveSpot(4, client);
-
-        System.out.println(system.spotsAvailable());
-
-        system.releaseSpot(4);
-
-        System.out.println(system.getSpot(4).getClient());
 
     }
 }
